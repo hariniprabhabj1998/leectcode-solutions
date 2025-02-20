36f8311c395c97bb5b0e140705a2b059a90c8763
Harini <hariniprabha.bj@gmail.com>
2025-02-19 23:58:08 -0500

    Two sum problem updated to better complexity

--- leetcode_project_solutions/src/leetcode_project_solutions/TwoSum.java
+++ leetcode_project_solutions/src/leetcode_project_solutions/TwoSum.java
@@ -1,20 +1,33 @@
 package leetcode_project_solutions;
 
 
+import java.util.HashMap;
+import java.util.Arrays;
+
 
 public class TwoSum {
-	 public int[] twoSum(int[] nums, int target) {
-         for (int i=0;i<nums.length;i++){
-            for (int j=i+1; j<nums.length; j++){
-                if (nums[i]+nums[j] == target){
-                 
-                return new int[]{i,j};
-            }
-            }
-         }
-         return new int[]{};
-    }
-    
-         }
+	public static void main(String[] args) {
+		TwoSum twoSumProblem = new TwoSum();
+		int[] arr = new int[] { 3, 2, 4, 1 };
+		int target = 6;
+		System.out.println(Arrays.toString(twoSumProblem.twoSum(arr, target)));
 
+//if you don't use Arrays.toString it prints the hashcode(hexa decimal representation of memory address) 
+//of the array  unless you explicitly convert to a readable format
+//System.out.println - does not directly print array elements
 
+	}
+
+	public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i=0; i<nums.length;i++){
+          int diff = target - nums[i];
+          if(hm.containsKey(diff)){
+               return new int[]{hm.get(diff), i};
+          }
+           hm.put(nums[i],i);
+        }
+        return null;
+   }
+}
+//Ctrl+shift+F - to format the code
